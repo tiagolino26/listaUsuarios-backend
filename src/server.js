48b5dca82f322fb9a091046aabb233297e83056b
tiagolino26 <tiagolino2026@gmail.com>
2026-06-02 16:43:12 -0300

    inserido a porta pelo render

--- src/server.js
+++ src/server.js
@@ -55,7 +55,7 @@ fastify.get("/", async function handler(request, reply) {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({ port: process.env.PORT || 3000 });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
